73886129483deb4a0139cb6e4f8da1274e013e68
Laura Avila <lauritaila10gmail.com>
2026-01-05 16:35:47 -0400

    FEAT: add Gemini module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { GeminiModule } from './gemini/gemini.module';
 
 @Module({
-  imports: [],
+  imports: [GeminiModule],
   controllers: [],
   providers: [],
 })
