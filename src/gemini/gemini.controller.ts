8a76a81528fd98c9593498423e34816b7199faa2
Laura Avila <lauritaila10gmail.com>
2026-01-06 15:22:19 -0400

    feat: integrate Google GenAI and add basic prompt functionality
    - Added @google/genai dependency for AI integration.
    - Introduced BasicPromptDto for validating prompt input.
    - Updated GeminiController to handle POST requests for basic prompts.
    - Implemented basicPrompt method in GeminiService to interact with Google GenAI.
    - Configured global validation pipes in main.ts for input validation.
    - Set up ConfigModule for environment variable management.

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
+import { BasicPromptDto } from './dtos/basic-prompt.dto';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
 
-  @Get()
-  getHello(): string {
-    return 'hola mundo';
+  @Post('basic-prompt')
+  async basicPrompt(@Body() basicPrompt: BasicPromptDto) {
+    return this.geminiService.basicPrompt(basicPrompt);
   }
 }
--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { GoogleGenAI } from '@google/genai';
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+    private ai = new GoogleGenAI({
+        apiKey: process.env.GEMINI_API_KEY,
+    });
+
+    async basicPrompt(basicPrompt: BasicPromptDto) {
+        const response = await this.ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: basicPrompt.prompt,
+            config:{
+                systemInstruction: "responde en ingles de manera concisa y clara. en formato markdown",
+            }
+        });
+        console.log(response.text);
+        return { response: response.text };
+    }
+}
