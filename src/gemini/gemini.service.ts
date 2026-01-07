9e655d372bd30c21e3f3048a826a93280a126f91
Laura Avila <lauritaila10gmail.com>
2026-01-07 10:49:25 -0400

    feat: refactor basicPrompt method and implement basicPromptUseCase for improved functionality

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
 import { GoogleGenAI } from '@google/genai';
+import { basicPromptUseCase } from './use-cases/basic-prompt.use-case';
 
 @Injectable()
 export class GeminiService {
@@ -9,14 +10,6 @@ export class GeminiService {
     });
 
     async basicPrompt(basicPrompt: BasicPromptDto) {
-        const response = await this.ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: basicPrompt.prompt,
-            config:{
-                systemInstruction: "responde en ingles de manera concisa y clara. en formato markdown",
-            }
-        });
-        console.log(response.text);
-        return { response: response.text };
+        return basicPromptUseCase(this.ai, basicPrompt);
     }
 }
